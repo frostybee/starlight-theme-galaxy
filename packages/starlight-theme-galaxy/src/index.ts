47a60b53d6141b8450c3463acc4f15c1b0a90599
frostybee <rsleiman2@gmail.com>
2025-05-19 20:37:27 -0400

    Add galaxy theme custom CSS

--- packages/starlight-theme-galaxy/src/index.ts
+++ packages/starlight-theme-galaxy/src/index.ts
@@ -1,19 +1,11 @@
 import type { StarlightPlugin } from '@astrojs/starlight/types'
 
-export default function starlightThemeGalaxy(): StarlightPlugin {
+export default function starlightThemeGalaxyPlugin(): StarlightPlugin {
   return {
     name: 'starlight-theme-galaxy',
     hooks: {
       'config:setup'({ config, logger, updateConfig }) {
-        /**
-         * This is the entry point of your Starlight plugin.
-         * The `config:setup` hook is called when Starlight is initialized (during the Astro `astro:config:setup`
-         * integration hook).
-         * To learn more about the Starlight plugin API and all available options in this hook, check the Starlight
-         * plugins reference.
-         *
-         * @see https://starlight.astro.build/reference/plugins/
-         */
+
         logger.info('Hello from the starlight-theme-galaxy plugin!')
 
         /**
@@ -24,7 +16,10 @@ export default function starlightThemeGalaxy(): StarlightPlugin {
          * @see https://starlight.astro.build/reference/configuration/#customcss
          */
         updateConfig({
-          customCss: [...(config.customCss ?? []), 'starlight-theme-galaxy/styles'],
+          customCss: [
+            'starlight-theme-galaxy/styles/index.css',
+            ...(config.customCss ?? []),
+          ],
         })
       },
     },
